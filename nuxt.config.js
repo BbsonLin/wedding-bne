5c8def97ec112ac5234d97ba338db1260211a3a4
BobsonLin <bobson801104@gmail.com>
2021-05-18 20:53:18 +0800

    Use font GreatVibes and add tailwind.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,14 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheets',
+        href:
+          'https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
