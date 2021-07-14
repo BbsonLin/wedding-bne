f9bfe1dd684cd8a7a2b20a12a3553a39910370b7
BobsonLin <bobson801104@gmail.com>
2021-07-14 08:42:29 +0800

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <div id="header">
       <nav class="fixed flex justify-center w-screen bg-transparent py-6">
-        <h1 class="text-5xl md:text-6xl lg:text-7xl">Bobson & Elva</h1>
+        <h1 class="transition-all" :class="headerFontSize">Bobson & Elva</h1>
       </nav>
     </div>
 
@@ -24,7 +24,31 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      headerFontSize: 'text-5xl',
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollShrinkHeader)
+  },
+  methods: {
+    scrollShrinkHeader() {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        this.headerFontSize = 'text-3xl md:text-4xl lg:text-5xl'
+        // document.querySelector('#header h1').classList.add('text-3xl')
+        // document.querySelector('#header h1').style.fontSize = '30px'
+        // document.getElementById('header').style.fontSize = '30px'
+      } else {
+        this.headerFontSize = 'text-5xl md:text-6xl lg:text-7xl'
+      }
+    },
+  },
+}
 </script>
 
 <style>
