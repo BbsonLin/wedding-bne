b331b8b860480a50c3aee49fb89d3a2cef16842c
Bobson Lin <bobson801104@gmail.com>
2021-10-06 17:02:46 +0800

    Add bride-cake page

--- pages/bride-cake/_id.vue
+++ pages/bride-cake/_id.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
-    <p>Cake</p>
+    <p>Cake {{ $route.params.id }}</p>
+    <p>{{ brideCakeList }}</p>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      brideCakeList: [],
+    }
+  },
+  mounted() {
+    console.log(this.brideCakeList)
+    this.readFromRealtimeDb()
+  },
+  methods: {
+    readFromRealtimeDb() {
+      // const listRef =
+      this.$fire.database.ref('list/').once('value', function (snapshot) {
+        const data = snapshot.val()
+        console.log(data)
+        console.log(this.brideCakeList)
+        this.brideCakeList = snapshot.val()
+      })
+      // console.log('listRef :>> ', listRef)
+      // try {
+      //   const snapshot = await listRef.child(0)
+      //   alert(snapshot.get())
+      //   alert(snapshot.val().message)
+      // } catch (e) {
+      //   alert(e)
+      // }
+    },
+  },
+}
 </script>
